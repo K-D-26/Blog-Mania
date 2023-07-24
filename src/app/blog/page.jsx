5410ccc1f4561650eb42c375d0b7e066126848e9
K-D-26 <daglikavan26@gmail.com>
2023-07-24 15:52:39 +0530

    blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -17,11 +17,12 @@ async function getData() {
 
 const Blog = async () => {
 
-  const data = await getData()
+  // const data = await getData()
+  const data = [];
 
   return (
     <div className={styles.mainContainer}>
-      {data.map((item) => (
+      {data?.map((item) => (
         <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
           <div className={styles.imageContainer}>
             <Image
