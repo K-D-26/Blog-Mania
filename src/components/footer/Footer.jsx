bc2c61fccb3d7e07363bf51a7adcba0e59abf35a
K-D-26 <daglikavan26@gmail.com>
2023-07-24 16:22:01 +0530

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,12 @@ import styles from './Footer.module.css'
 import Image from 'next/image'
 
 const Footer = () => {
+
+    const currentDate = new Date();
+
     return (
         <div className={styles.container}>
-            <div>©2023 Blog-Mania. All rights reserved.</div>
+            <div>©{currentDate.getFullYear()} Blog-Mania. All rights reserved.</div>
             <div className={styles.social}>
                 <Image src='/1.png' width={15} height={15} alt='facebook-handle' className={styles.icon} />
                 <Image src='/2.png' width={15} height={15} alt='instagram-handle' className={styles.icon} />
